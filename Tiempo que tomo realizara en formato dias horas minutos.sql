c014de84c9e8ec6da93c4c3d9c35475f98456991
Jose Luis Riffo Toro <jriffo@scj.gob.cl>
2021-03-05 15:49:59 -0300

    Se agrega script sql agrega nuevos campos historico de instancia de tarea

---
+++ Tiempo que tomo realizara en formato dias horas minutos.sql
@@ -0,0 +1,8 @@
+alter table sgdp."SGDP_HISTORICO_DE_INST_DE_TAREAS"
+add column "N_DIAS_OCUPADOS" int2 ;
+
+alter table sgdp."SGDP_HISTORICO_DE_INST_DE_TAREAS"
+add column "N_HORAS_OCUPADAS" int2 int2 ;
+
+alter table sgdp."SGDP_HISTORICO_DE_INST_DE_TAREAS"
+add column "N_MINUTOS_OCUPADOS" int2 ;
